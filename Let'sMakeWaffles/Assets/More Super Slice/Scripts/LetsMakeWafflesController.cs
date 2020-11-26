e54a33f2362b4da63045680eacb7b617ac9a18ae
Hamza <hamza.jahangir@geniteam.com>
2020-11-26 17:58:36 +0500

    Initial Commit
    - Background.
    - Play button location
    - Scene transitions

--- Let'sMakeWaffles/Assets/More Super Slice/Scripts/LetsMakeWafflesController.cs
+++ Let'sMakeWaffles/Assets/More Super Slice/Scripts/LetsMakeWafflesController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MoreSuperSliceController : MonoBehaviour {
+public class LetsMakeWafflesController : MonoBehaviour {
 
 	public GameObject[] answers;
 	public Animator zapeAnimator;
